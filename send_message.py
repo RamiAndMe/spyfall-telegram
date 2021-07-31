d890db181e3450245d7880fb6bead1230e89ff40
RamoAndMe <83104627+RamiAndMe@users.noreply.github.com>
2021-07-31 07:40:52 +0400

    HJH

--- send_message.py
+++ send_message.py
@@ -5,7 +5,7 @@ import requests
 from pprint import pprint
 
 # Setting up some variables...
-BOT_TOKEN = '<1939656470:AAGPRsDVZMqvKBlhul_Bd_1Xxgen-K8DHzg'
+BOT_TOKEN = '1939656470:AAGPRsDVZMqvKBlhul_Bd_1Xxgen-K8DHzg'
 BOT_URL = 'https://api.telegram.org/cesusbot' + BOT_TOKEN + '/'
 
 
